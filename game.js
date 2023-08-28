5dd2dd8a5354f415beae7aee52a35d2ce66f2ddc
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-08-28 20:12:42 +0530

    completed

--- game.js
+++ game.js
@@ -9,7 +9,6 @@ var level = 0;
 
 $(document).keypress(function() {
   if (!started) {
-
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
@@ -23,10 +22,43 @@ $(".btn").click(function() {
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+
+}
 
 function nextSequence() {
 
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
 
@@ -48,4 +80,11 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function startOver() {
+
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
